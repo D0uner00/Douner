43276afb0182393cd22cf7ff50b72c85c42e1da0
do <do@do.kr>
2026-03-26 09:16:05 +0900

    player.h 주석 정리

--- Douner/Player.h
+++ Douner/Player.h
@@ -1,7 +1,8 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 #include "global.h"
-// 1. 원본 달리기 자르기 정보 (유지)
+
+// 달리기 자르기 정보
 #define RUN_CROP_X 43 
 #define RUN_CROP_Y 33
 #define RUN_SRC_W 45
@@ -9,20 +10,17 @@
 #define RUN_DEST_W 105
 #define RUN_DEST_H 130 
 
-// ★ 2. 점프 시트 자르기 정보 (새로 추정해서 입력)
-// 점프 시트의 캐릭터들이 키가 더 크고 넓기 때문에 자르기 영역을 더 크게 잡아야 합니다.
-// (실행해보고 눈으로 확인하며 미세조정이 필요합니다)
+//점프 자르기 정보 
 #define JUMP_CROP_X 32
 #define JUMP_CROP_Y 10
 #define JUMP_SRC_W 64
-#define JUMP_SRC_H 90 // 달리기보다 더 크고 긴 영역을 자릅니다.
-
-// 달리기 비율에 맞춰 점프 시트를 화면에 뻥튀기할 크기
+#define JUMP_SRC_H 90
 #define JUMP_DEST_W 140
-#define JUMP_DEST_H 260 // 달리기보다 약 2배 크게 그림
+#define JUMP_DEST_H 260
 
+//프레임 수
 #define MAX_RUN_FRAMES 10
-#define MAX_JUMP_FRAMES 6 // 점프 시트도 10프레임으로 보임
+#define MAX_JUMP_FRAMES 6
 
 typedef struct player {
 	int runFrame;
